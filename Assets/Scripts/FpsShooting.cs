b9abd52fe42707bbb4ac2238429a1cd8ca0231ac
Gaurav0818 <80704349+Gaurav0818@users.noreply.github.com>
2022-01-02 10:21:27 +0530

    effect Assets Added

--- Assets/Scripts/FpsShooting.cs
+++ Assets/Scripts/FpsShooting.cs
@@ -7,6 +7,7 @@ public class FpsShooting : MonoBehaviour
     public GameObject bulletPrefab;
     public PlayerController player; 
     public Transform bulletLocation;
+    public ParticleSystem MuzzleFlash;
     public float shootPower = 100f;
 
     void start()
@@ -20,8 +21,9 @@ public class FpsShooting : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.Mouse0) && player.canShoot)
         {
-            Instantiate(bulletPrefab, bulletLocation.position, bulletLocation.rotation).
-                    GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * shootPower);
+            MuzzleFlash.Play();
+           //Instantiate(bulletPrefab, bulletLocation.position, bulletLocation.rotation).
+           //        GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * shootPower);
         }
     }
 }
